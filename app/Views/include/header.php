06a48b6cee57c8631cca37eda878465ca996cbfb
pesova <pesova13@gmail.com>
2020-11-18 13:05:16 +0100

    added error page and heart icon

--- app/Views/include/header.php
+++ app/Views/include/header.php
@@ -6,7 +6,11 @@
     <title><?php echo SITENAME; ?></title>
     
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/font-awesome.css">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/line-awesome.css">
+
+    
 
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
 </head>
--- app/Views/posts/index.php
+++ app/Views/posts/index.php
@@ -11,13 +11,25 @@
     </div>
   </div>
   <?php foreach($data['posts'] as $post) : ?>
-    <div class="card card-body mb-3">
+    <div class="card card-body bg-light mb-3">
       <h4 class="card-title"><?php echo $post->title; ?></h4>
       <div class="bg-light p-2 mb-3">
-        Written by <?php echo $post->name; ?> on <?php echo $post->postCreated; ?>
+        Written by <?php echo $post->name; ?> on <?php echo date("h:i a . d M y ", strtotime($post->postCreated)); ?>
       </div>
       <p class="card-text"><?php echo $post->body; ?></p>
-      <a href="<?php echo URLROOT; ?>/posts/show/<?php echo $post->postId; ?>" class="btn btn-dark">More</a>
+
+     <span>
+        <div class="row">
+          <div class="col-6">
+              <a href="<?php echo URLROOT; ?>/posts/show/<?php echo $post->postId; ?>" class="btn btn-dark btn-block">View Post</a>
+          </div>
+            <div class="col-6">
+              <i title="Heart" id="heart_icon" style="font-size: 2rem;" class="la la-heart-o pull-right"></i>
+            </div>
+        </div>
+     </span>
+        <!-- <a href="<?php echo URLROOT; ?>/posts/show/<?php echo $post->postId; ?>" class="btn btn-dark">More</a> -->
+
     </div>
   <?php endforeach; ?>
   <?php require APPROOT . '/Views/include/footer.php';?>
